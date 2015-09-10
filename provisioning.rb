0a85a69ddca27581695faad486fecc8c48961e4b
Tsuyoshi Ushio <ushio@simplearchitect.com>
2015-09-10 20:37:36 +0900

    change VM size and minor revision

--- provisioning.rb
+++ provisioning.rb
@@ -3,9 +3,9 @@ with_driver 'azure'
 
 machine_options = {
   :bootstrap_options => {
-    :cloud_service_name => 'pendricachefcemo01',
+    :cloud_service_name => 'pendricachefdemo01',
     :storage_account_name => 'pendricachefdemo01',
-    :vm_size => "Small",
+    :vm_size => "Standard_D11",
     :location => 'East Asia'
   },
   :image_id => 'b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_2-LTS-amd64-server-20150706-en-us-30GB',
